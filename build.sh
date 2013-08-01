ead9f852053a7d53ce9409b8df12443bdb411028
Jordan Curzon <curzonj@gmail.com>
2013-08-01 12:21:22 -0700

    resulting disk image boots

--- build.sh
+++ build.sh
@@ -40,16 +40,14 @@ cp /var/cache/apt/archives/*.deb /mnt/var/cache/apt/archives/
 #(Enter the items you want to select, separated by spaces.)
 #
 #GRUB install devices:
-yes 3 | env LC_ALL=C chroot /mnt bash -c 'apt-get update && apt-get -y install linux-generic'
+yes 2 | env LC_ALL=C chroot /mnt bash -c 'apt-get update && apt-get -y install linux-generic'
+
+# We install build-essential because we need it for the virtulabox guest
+# additions and vagrant is a dev env, so you're probably going to be compiling
+# anyways.
+env LC_ALL=C chroot /mnt apt-get -y install curl wget openssh-server sudo net-tools resolvconf build-essential virtualbox-guest-dkms
 
 env LC_ALL=C chroot /mnt bash <<"END_CHROOT"
-set -e
-set -x
-
-apt-get -y install curl wget openssh-server sudo
-
-echo vagrant-core > /etc/hostname
-
 echo "root:vagrant"|chpasswd
 
 useradd vagrant
@@ -58,6 +56,8 @@ echo "vagrant:vagrant"|chpasswd
 addgroup vagrant adm
 addgroup vagrant sudo
 END_CHROOT
+
+echo vagrant-core > /mnt/etc/hostname
 
 mkdir -p /mnt/root/.ssh
 echo 'ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ== vagrant insecure public key' > /mnt/root/.ssh/authorized_keys
@@ -85,6 +85,8 @@ umount /mnt/sys
 umount /mnt/dev
 
 rm /mnt/etc/resolv.conf
+
+exit 0
 
 apt-get -y install extlinux
 
